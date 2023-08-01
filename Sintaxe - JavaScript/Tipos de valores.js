066b2f2780dfdfc7bbb4e3bc691dd1c1aba2ba62
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 10:55:10 -0300

    add content

--- Sintaxe - JavaScript/Tipos de valores.js
+++ Sintaxe - JavaScript/Tipos de valores.js
@@ -10,12 +10,22 @@ Valores fixos são chamados de 'Literais'.
 
 Os valores das variáveis ​​são chamados de 'Variáveis'.*/
 
-console.log("\n\tValores Literais do JavaScript")
+console.log("\n\tValores Literais do JavaScript:\n")
 
 /*Literais JavaScript:
 
 As duas regras de sintaxe mais importantes para valores fixos são:
 
-1. Os números são escritos com ou sem casas decimais:
-*/
+1. Os números são escritos com ou sem casas decimais:*/
+
+var x = 10.50;
+var y = 1001;
+
+console.log("Número com casa decimal:", x)
+console.log("Número sem casa decimal:", y, "\n")
+
+/*2. Strings são textos, escritos entre aspas duplas ou simples:*/
+
+console.log("Frase com aspas dupla.")
+console.log('Frase com aspas simples.')
 
