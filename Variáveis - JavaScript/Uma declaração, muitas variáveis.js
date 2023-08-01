f640cb6cd2da954a8563cab5ea8df581462eb8ca
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 13:17:47 -0300

    add content

---
+++ Variáveis - JavaScript/Uma declaração, muitas variáveis.js
@@ -0,0 +1,21 @@
+console.log("\n\tTipos de Uma declaração, muitas variáveis - JavaScript:\n");
+
+/*Você pode declarar muitas variáveis em um instrução.
+
+Inicie a instrução 'let' w separe as variáveis por uma vírgula:*/
+
+let pessoa1 = "Théo Bortoletto", carroNome1 = "Peugeot", preço1 = 17000;
+
+/*Uma declaração pode abranger várias linhas:*/
+
+let pessoa2 = "Flavia Sarahim", 
+    carroNome2 = "Volkswagen", 
+    preço2 = 60000;
+
+console.log("Informações da pessoa 1:\n\nNome:", pessoa1, 
+                                     "\nCarro:", carroNome1, 
+                                     "\nPreço:", preço1, "\n")
+                                     
+console.log("Informações da pessoa 2:\n\nNome:", pessoa2, "\nCarro:", carroNome2, "\nPreço:", preço2, "\n")
+
+
