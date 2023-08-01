081a4e8849c94a67c4dac56380dcf1b704160c46
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 13:10:05 -0300

    add file

---
+++ Variáveis - JavaScript/Declarando uma variável.js
@@ -0,0 +1,21 @@
+console.log("\n\tDeclarando uma variável - JavaScript:\n");
+
+/*Criar uma variável em JavaScript é 
+ de "declarar" uma variável.
+
+Você declara uma variável JavaScript 
+com var ou a  palavra-chave let:*/
+
+var carName;
+let carName;
+
+/*Após a declaração, a variável não tem valor 
+(tecnicamente é undefined - indefinida).
+
+Para atribuir um valor à variável, use o sinal de igual:*/
+
+carName = "Volvo"
+
+/*Você também pode atribuir um valor à variável ao declará-la:*/
+
+let CarName = "Volvo";
