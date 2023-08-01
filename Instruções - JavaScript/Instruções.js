6f314acc7c55738295ed79aa374b674699f4ec4c
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 09:25:05 -0300

    add content

--- Instruções - JavaScript/Instruções.js
+++ Instruções - JavaScript/Instruções.js
@@ -1,8 +1,19 @@
 console.log("\n\tInstruções - JavaScript: \n");
 
-let x, y, z; 
+let x, y, z; //* Instrução 1
 
-x = 5;
-y = 6;
+x = 5; //* Instrução 2
+y = 6; //* Instrução 3 
+z = x + y; //* Instrução 4
+
+/*Programas JavaScript:
+
+Um programa de computador é uma lista de "instruções" a serem "executadas" por um computador.
+
+Em uma linguagem de programação, essas instruções de programação são chamadas de instruções .
+
+Um programa JavaScript é uma lista de instruções de programação .*/
+
+
 
 
