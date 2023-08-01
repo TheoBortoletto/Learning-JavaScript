ac118bb401de2c05a6c3fcc638d5f8bca6ff5b01
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 14:03:29 -0300

    add content

--- Variáveis - JavaScript/Re-declarando variáveis ​​JavaScript.js
+++ Variáveis - JavaScript/Re-declarando variáveis ​​JavaScript.js
@@ -1,3 +1,23 @@
 console.log("\n\tRe-declarando variáveis JavaScript - JavaScript:\n");
 
+/*Se você declarar novamente uma variável JavaScript 
+declarada com var, ela não perderá seu valor.
+
+A variável 'carName' ainda terá o valor "Volvo" 
+após a execução destas instruções:*/
+
+var carName = "Volvo";
+var carName;
+
+console.log(carName)
+
+/*Observação:
+
+Você não pode declarar novamente uma variável 
+declarada com let ou const.
+
+Isso não vai funcionar:*/
+
+// let nome = "Théo Bortoletto";
+// let nome;
 
