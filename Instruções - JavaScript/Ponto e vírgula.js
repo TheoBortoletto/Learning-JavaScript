4969ee8df6e5773251f49ef778b78b5762c8f36d
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 10:19:44 -0300

    add content

--- Instruções - JavaScript/Ponto e vírgula.js
+++ Instruções - JavaScript/Ponto e vírgula.js
@@ -15,3 +15,17 @@ c = a + b; //* Atribuir a soma de 'a' e 'b' pata a variável 'c'
 console.log("Valor de a:", a);
 console.log("Valor de b:", b);
 console.log("Valor de c (a + b):", c)
+
+console.log("\n\tInstrução em uma linha: \n")
+
+/*Quando separados por ponto e vírgula, são permitidas várias instruções em uma linha:*/
+
+let val1, val2, val3;
+
+val1 = 1; val2 = 2; val3 = val1 + val2;
+
+console.log("Valor de val1:", val1);
+console.log("Valor de val2:", val2);
+console.log("Valor de val3 (val1 + val2):", val3);
+
+
