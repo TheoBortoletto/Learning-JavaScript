940c59822e4b27bf985c8bbabf7cff381897c803
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 10:48:45 -0300

    add content

---
+++ Sintaxe - JavaScript/Tipos de valores.js
@@ -0,0 +1,21 @@
+console.log("\n\tTipos de valores - JavaScript: \n");
+
+/*Valores JavaScript:
+A sintaxe JavaScript define dois tipos de valores:
+
+- valores fixos
+- valores variáveis
+
+Valores fixos são chamados de 'Literais'.
+
+Os valores das variáveis ​​são chamados de 'Variáveis'.*/
+
+console.log("\n\tValores Literais do JavaScript")
+
+/*Literais JavaScript:
+
+As duas regras de sintaxe mais importantes para valores fixos são:
+
+1. Os números são escritos com ou sem casas decimais:
+*/
+
