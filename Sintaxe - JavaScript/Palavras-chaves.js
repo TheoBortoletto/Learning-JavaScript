034eb6c04cb152c1d08c490ab03e028dd517770e
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 11:21:50 -0300

    add content

---
+++ Sintaxe - JavaScript/Palavras-chaves.js
@@ -0,0 +1,15 @@
+console.log("\n\tPalavras-chaves - JavaScript: \n");
+
+/*Palavras-chave JavaScript:
+
+Palavras-chave JavaScript são usadas para 
+identificar ações a serem executadas.
+
+A palavra-chave 'let' diz ao navegador 
+para criar variáveis:*/
+
+let x, y;
+x = 5 + 6;
+y = x * 10;
+
+
