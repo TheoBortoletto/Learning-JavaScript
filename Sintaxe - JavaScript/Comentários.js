1fd3ca37be6f7cbe81157d18eb5c932700fdf728
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 12:06:43 -0300

    add content

---
+++ Sintaxe - JavaScript/Comentários.js
@@ -0,0 +1,14 @@
+console.log("\n\tComentários - JavaScript:\n");
+
+/*Comentários JavaScript
+Nem todas as instruções JavaScript são "executadas".*/
+
+//*Código após barras duplas // ou entre /*e*/ é tratado como um comentário .
+
+/* Os comentários são ignorados e não serão executados:*/
+
+let x = 5;   //* Será executado
+
+//* xs = 6;   Não será executado
+
+console.log(x)
