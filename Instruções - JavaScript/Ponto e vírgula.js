cf36603ef1aa20e3cf95a367b4301b2fe1fa3ec0
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 09:50:44 -0300

    add content

---
+++ Instruções - JavaScript/Ponto e vírgula.js
@@ -0,0 +1,17 @@
+console.log("\n\tPonto e vírgulas - JavaScript: \n");
+
+/*Ponto e Vírgula - ";"
+
+Ponto e vírgula separam instruções JavaScript.
+
+Adicione um ponto e vírgula no final de cada instrução executável:*/
+
+let a, b, c; //* Declarando 3 variáveis
+
+a = 10; //* Atribuir o valor de a para 10
+b = 5;  //* Atribuir o valor de b para 5
+c = a + b; //* Atribuir a soma de 'a' e 'b' pata a variável 'c'
+
+console.log("Valor de a:", a);
+console.log("Valor de b:", b);
+console.log("Valor de c (a + b):", c)
