4e12ccc360168b9a0112c437a73c8febcb1f5237
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 11:21:53 -0300

    add content

---
+++ Sintaxe - JavaScript/Operadores.js
@@ -0,0 +1,15 @@
+console.log("\n\tOperadoress - JavaScript: \n");
+
+/*JavaScript usa operadores aritméticos (+, -, *, /,) 
+para calcular valores:*/
+
+console.log("Exemplo de uso de operadores:", (5 + 6) * 10)
+
+/*JavaScript usa um operador de atribuição (=) 
+para atribuir valores a variáveis:*/
+
+let x, y;
+x = 2;
+y = 3;
+
+console.log("\nValor de x:", x, "e valor de y:", y)
