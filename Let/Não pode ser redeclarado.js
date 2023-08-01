52246b2cc71d4e6e4ab212444fe11ad04140c0e5
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 14:17:05 -0300

    add file

---
+++ Let/Não pode ser redeclarado.js
@@ -0,0 +1,8 @@
+console.log("\n\Let não pode ser redeclarado - JavaScript:\n");
+
+/*Variáveis ​​definidas com let não podem ser redeclaradas.
+
+Você não pode redeclarar acidentalmente uma variável declarada com let.*/
+
+/*let x = "Théo Bortoletto"
+let x = 0*/ //* NÃO PODE FAZER ISSO USANOD O LET, JA var SIM.
