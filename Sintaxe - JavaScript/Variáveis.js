1272af3b76f1bb57249bc8cbeff833cae35f27d3
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 11:04:27 -0300

    add content

---
+++ Sintaxe - JavaScript/Variáveis.js
@@ -0,0 +1,18 @@
+console.log("\n\tTipos de variáveis - JavaScript: \n");
+
+/*Em uma linguagem de programação, as variáveis​​são usadas 
+para armazenar valores de dados.
+
+JavaScript usa as palavras-chave var, lete constpara 
+declarar variáveis.
+
+Um sinal de igual é usado para atribuir valores a 
+variáveis.
+
+Neste exemplo, x é definido como uma variável. Então, 
+x é atribuído (dado) o valor 6:*/
+
+let x;
+x = 6;
+
+console.log("Valor da variável x:", xs)
