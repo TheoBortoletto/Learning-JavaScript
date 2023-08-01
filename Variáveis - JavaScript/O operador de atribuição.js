356c5142c6a64efd7a3283bfae45e04c1cb15ffd
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 13:04:20 -0300

    add content

--- Variáveis - JavaScript/O operador de atribuição.js
+++ Variáveis - JavaScript/O operador de atribuição.js
@@ -1,3 +1,18 @@
 console.log("\n\tO operador de atribuição - JavaScript:\n");
 
+/*Em JavaScript, o sinal de igual (=) 
+é um operador de "atribuição", não um operador "igual a".
 
+Isso é diferente da álgebra. O seguinte não faz sentido em álgebra:*/
+
+x = x + 5;
+
+/*Em JavaScript, porém, faz todo o sentido: 
+atribui o valor de x + 5 a x.
+
+(Ele calcula o valor de x + 5 e coloca o resultado em x. 
+O valor de x é incrementado em 5.)*/
+
+/*Observação:
+
+O operador "igual a" é escrito como '==' em JavaScript.*/
