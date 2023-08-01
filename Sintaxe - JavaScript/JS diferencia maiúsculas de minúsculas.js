6230a620caddba7bc56e3b62df414a43d8e63293
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-01 12:17:32 -0300

    add content

---
+++ Sintaxe - JavaScript/JS diferencia maiúsculas de minúsculas.js
@@ -0,0 +1,15 @@
+console.log("\n\tJavaScript diferencia maiúsculas de minúsculas - JavaScript:\n");
+
+/*Todos os identificadores JavaScript 
+diferenciam maiúsculas de minúsculas . 
+
+As variáveis lastName​​e lastname, 
+são duas variáveis ​​diferentes:*/
+
+let lastname, lastName;
+
+lastname = "Bortoletto"
+lastName = "Ghetti"
+
+console.log(lastname);
+console.log(lastName);
