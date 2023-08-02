fc23ce9f137b4639e8daabd97b99eb21aad6f7a8
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-02 11:20:08 -0300

    add content

--- Let/Redeclarando variáveis.js
+++ Let/Redeclarando variáveis.js
@@ -1,5 +1,7 @@
 console.log("\n\tRedeclarando Variáveis - JavaScript:\n");
 
+console.log("\tRedeclarando com  'var':\n")
+
 /*Redeclarar uma variável usando a palavra-chave 'var' 
 pode impor problemas.
 
@@ -17,12 +19,22 @@ console.log("Variável x fora de um bloco (escopo global):", x, "\n")
 console.log("Variável x após ser redeclarada no bloco:", x, "\n")
 //* Aqui o x é 2
 
+console.group("a")
+
 /*Redeclarar uma variável usando a palavra-chave 'let' 
 pode resolver esse problema.
 
 Redeclarar uma variável dentro de um bloco não 
 redeclarará a variável fora do bloco:*/
 
+let y = 10;
+//* Aqui o y é igual a 10
+console.log("Variável y fora de um bloco (escopo global):", y, "\n")
 
-
-
+{
+    let y = 2;
+    //* Aqui o y é igual a 2
+    console.log("Variável y dentro de um bloco:", y, "\n")
+}
+console.log("Variável y após ser redeclarada no bloco:", y, "\n")
+//* Aqui o y é igual a 10
