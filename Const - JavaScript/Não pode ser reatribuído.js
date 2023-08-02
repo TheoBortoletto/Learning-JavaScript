8dd6a5f5f434f47798828fc0bddef85c94cd0e27
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-02 13:46:11 -0300

    add content

--- Const - JavaScript/Não pode ser reatribuído.js
+++ Const - JavaScript/Não pode ser reatribuído.js
@@ -1,2 +1,9 @@
 console.log("\n\tConst não pode ser reatribuído:\n")
 
+/*Uma variável 'const' não pode ser reatribuída:*/
+
+const PI = 3.141592653589793;
+
+// PI = 3.14;    //* Isso gerará um erro
+// PI = PI + 10; //* Isso também gerará um erro
+
