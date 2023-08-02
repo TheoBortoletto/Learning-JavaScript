9405c3d59e5993d32a5140ec78e932ed2d80e308
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-02 13:46:08 -0300

    add content

---
+++ Const - JavaScript/Devem ser atribuídas.js
@@ -0,0 +1,10 @@
+console.log("\n\tVariáveis 'const' devem ser atribuídas:\n")
+
+/*As variáveis 'const' devem receber um valor 
+quando são declaradas:*/
+
+const PI = 3.14159265359;
+
+console.log("PI é igual a:", PI)
+
+/*FORMA INCORRETA*/
