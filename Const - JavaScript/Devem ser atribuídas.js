c72fe653e394980dc6565108ec4fecd39b627737
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-02 13:50:44 -0300

    add file

--- Const - JavaScript/Devem ser atribuídas.js
+++ Const - JavaScript/Devem ser atribuídas.js
@@ -12,3 +12,14 @@ console.log("PI é igual a:", PI)
 const PI;
 PI = 3.14159265359;*/
 
+/*QUANDO USAR O 'const':
+
+Sempre declare uma variável com 'const' quando 
+souber que o valor não deve ser alterado.
+
+Use 'const' quando você declarar:
+
+- Uma nova matriz
+- Um novo objeto
+- Uma nova função
+- Um novo RegExp*/
