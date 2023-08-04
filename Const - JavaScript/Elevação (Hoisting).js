621a6b0bca43fa845eba708701a6d3b9b40c1c00
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 14:01:40 -0300

    add file

--- Const - JavaScript/Elevação (Hoisting).js
+++ Const - JavaScript/Elevação (Hoisting).js
@@ -1,2 +1,11 @@
 console.log("\n\tElevação (Hoisting):\n")
 
+/*As variáveis ​​definidas com 'var' são levantadas 
+para o topo e podem ser inicializadas a qualquer momento.
+
+Significado: Você pode usar a variável antes de ser declarada:*/
+
+carName = "Volvo";
+var carName
+
+console.log(carName)
