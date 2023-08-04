8a3227bc8b324b23c59a87b437adc1b00f29422e
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 15:32:07 -0300

    add content

---
+++ Operadordes - JavaScript/Operadores aritméticos.js
@@ -0,0 +1,9 @@
+console.log("\n\tOperadores aritméticos:\n");
+
+/*Operadores aritméticos são usados para realizar aritmétca em números:*/
+
+let a = 3;
+let x = (100 + 50) * a;
+
+console.log("Valor de a:", a);
+console.log("Valor de x: (100 + 50) * a:", x);
