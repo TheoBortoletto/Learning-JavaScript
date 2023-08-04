b18e9d8057a04d48fcd392714eaddf08f8ce593d
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 15:26:59 -0300

    add content

---
+++ Operadordes - JavaScript/Multiplicação.js
@@ -0,0 +1,9 @@
+console.log("\n\tOperador de Multiplicação (*):\n");
+
+/*O operador de munltiplicação (*) multiplica números:*/
+
+let x = 5;
+let y = 5;
+mul = x * y;
+
+console.log("Multiplicação dos valores:", mul);
