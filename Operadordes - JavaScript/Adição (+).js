0f376dbf81e0d57cd9746f2970232f2e8ba76e2d
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 14:14:09 -0300

    add content

---
+++ Operadordes - JavaScript/Adição (+).js
@@ -0,0 +1,9 @@
+console.log("\n\tOperador de Adição:\n");
+
+/*O Operador de adição (+) adiciona números:*/
+
+let x = 5;
+let y = 30;
+let sum = x + y;
+
+console.log("Valor de x:", x, "\nVAlor de y:", y, "\nSoma dos valores:", sum);
