1bc1abdf730be8b27b64a48ff7aca839eb246ed7
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 14:08:57 -0300

    add content

--- Operadordes - JavaScript/Atribuição (=).js
+++ Operadordes - JavaScript/Atribuição (=).js
@@ -1,1 +1,11 @@
-console.log("")
+console.log("\n\tOperador de atribuição (=):\n");
+
+/*O Operador de Atribuição (=) atribui um valor a uma variável:*/
+
+let x = 10;
+let y = 2;
+let z = x + y;
+
+console.log("Valor da variável x:", x);
+console.log("Valor da variável y:", y);
+console.log("Valor da variável z:", z);
