b974a06b03cb92ee09a806482fb3ad481d8c7871
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 11:54:34 -0300

    add content

--- Const - JavaScript/Redeclarando const.js
+++ Const - JavaScript/Redeclarando const.js
@@ -18,3 +18,35 @@ const x = 2;   // Não é permitido
 }
 */
 
+//* Não é permitido reatribuir uma variável const existente, no mesmo escopo:
+
+
+/*
+const x = 2;     // Allowed
+x = 2;           // Not allowed
+var x = 2;       // Not allowed
+let x = 2;       // Not allowed
+const x = 2;     // Not allowed
+
+{
+  const x = 2;   // Allowed
+  x = 2;         // Not allowed
+  var x = 2;     // Not allowed
+  let x = 2;     // Not allowed
+  const x = 2;   // Not allowed
+}
+*/
+
+//* É permitido redeclarar uma variável com const, em outro escopo, ou em outro bloco:
+
+/*
+const x = 2;       // Allowed
+
+{
+  const x = 3;   // Allowed
+}
+
+{
+  const x = 4;   // Allowed
+}
+*/
