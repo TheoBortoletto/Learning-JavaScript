fe3d5702136f2b83c3cc1ec1835fbeec0303fc8b
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 15:38:23 -0300

    add content

---
+++ Operadordes - JavaScript/Operadores de atribuição.js
@@ -0,0 +1,22 @@
+console.log("\n\tOperadores de atribuição:\n");
+
+/*Operadores de atribuição atribuem valores a variáveis JavaScript.
+
+O operador de atribuição de adição (+=) adiciona um valor a uma variável:*/
+
+let x = 10;
+x += 5;
+
+console.log("Valor de x com a atribuição feita:", x);
+
+/*OUTROS EXEMPLOS:
+
+Operator:	Example:    Same As:
+
+=	        x = y	    x = y
++=	        x += y	    x = x + y
+-=	        x -= y	    x = x - y
+*=	        x *= y	    x = x * y
+/=	        x /= y	    x = x / y
+%=	        x %= y	    x = x % y
+**=	        x **= y	    x = x ** y*/
