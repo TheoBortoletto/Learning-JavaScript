63867b72775ef47b5f8b26bcd96367f5be2ebc34
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-04 15:40:47 -0300

    add file

---
+++ Operadordes - JavaScript/Operadores de comparação.js
@@ -0,0 +1,17 @@
+console.log("\n\tOperadores de comparação:\n");
+
+/*
+SÃO ELES:
+
+Operator	Description
+==	        equal to
+===	        equal value and equal type
+!=	        not equal
+!==	        not equal value or not equal type
+>	        greater than
+<	        less than
+>=	        greater than or equal to
+<=	        less than or equal to
+?	        ternary operator
+
+*/
