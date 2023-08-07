04b68841541235b23bc2bcdf5fe5688f033cce51
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-07 11:38:32 -0300

    add content

---
+++ Operadordes - JavaScript/Adição de string.js
@@ -0,0 +1,15 @@
+console.log("\n\tAdição de string:\n");
+
+let fname = "Théo";
+let lname = "Bortoletto";
+let fullname = fname + " " + lname;
+
+console.log("Nome:", fullname);
+
+/*O operador de atribuição também pode ser 
+usado para adicionar (concatenar) strings:*/
+
+let txt1 = "Mas que ";
+txt1 += "dia lindo";
+
+console.log("Frase:", txt1);
