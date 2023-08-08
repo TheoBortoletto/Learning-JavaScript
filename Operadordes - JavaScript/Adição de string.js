119e4df03ad0418c879817c7a782434df47585d3
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-08 09:37:22 -0300

    add content

--- Operadordes - JavaScript/Adição de string.js
+++ Operadordes - JavaScript/Adição de string.js
@@ -1,5 +1,7 @@
 console.log("\n\tAdição de string:\n");
 
+/*O símbolo '+' também pode ser usado para adicionar (concatenar) strings:*/
+
 let fname = "Théo";
 let lname = "Bortoletto";
 let fullname = fname + " " + lname;
@@ -12,4 +14,10 @@ usado para adicionar (concatenar) strings:*/
 let txt1 = "Mas que ";
 txt1 += "dia lindo";
 
-console.log("Frase:", txt1);
+console.log("Frase:", txt1, "\n");
+
+let text1 = "Eduarda";
+let text2 = "Medeiros";
+let text3 = text1 + " " + text2;
+
+console.log(text3)
