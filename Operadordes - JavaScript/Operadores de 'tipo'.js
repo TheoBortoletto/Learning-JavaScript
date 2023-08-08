d6fdfbcc7593d39f73ec44d945576291f26e8aee
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-08 13:12:14 -0300

    add content

--- Operadordes - JavaScript/Operadores de 'tipo'.js
+++ Operadordes - JavaScript/Operadores de 'tipo'.js
@@ -1,2 +1,8 @@
 console.log("\n\tOperadores de 'tipo':\n");
 
+/*SÃO ELES:
+
+OPERADOR:    DESCRIÇÃO:
+
+typeof       Retorna o tipo da variável
+instaceof    Retorna TRUE se o objeto for uma instância de tipo de objeto*/
