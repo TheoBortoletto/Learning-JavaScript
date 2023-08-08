f2abf17e80b1b1fed97b84b710a48c04e4ebcbe0
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-08 13:29:32 -0300

    add file

--- Aritmética - JavaScript/Operadores aritméticos.js
+++ Aritmética - JavaScript/Operadores aritméticos.js
@@ -1,2 +1,14 @@
 console.log("\n\tOperadores Aritméticos: \n");
 
+/*Operadores aritméticos executam aritmética em números (literais ou variáveis).
+
+Operator	Description
++	        Addition
+-	        Subtraction
+*	        Multiplication
+**	        Exponentiation (ES2016)
+/	        Division
+%	        Modulus (Remainder)
+++	        Increment
+--	        Decrement*/
+
