692438dc6c2b0f6721ea2cfec635e4c12c5e2644
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-08 10:21:10 -0300

    add contetn

---
+++ Operadordes - JavaScript/Adicionando strings e números.js
@@ -0,0 +1,12 @@
+console.log("\n\tAdicionando Strings e Números - JavaScript:");
+
+/*Adicionar dois números retornará a soma, 
+mas adicionar um número e uma string retornará uma string:*/
+
+let x = 5 + 5;
+let y = "5" + 5
+let z = "Hello" + 5;
+
+console.log(x);
+console.log(y);
+console.log(z);
