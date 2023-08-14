923ea3c2254ce575d6576f7ff583ca38f8fa840d
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-08-14 09:29:44 -0300

    add content

--- Aritmética - JavaScript/Operações aritmétcas.js
+++ Aritmética - JavaScript/Operações aritmétcas.js
@@ -1,2 +1,24 @@
 console.log("\n\tOperações aritmétcas: \n");
 
+/*Uma operação aritmétca opera em dois números
+
+Os dois podem ser literais:*/
+
+let x = 100 + 20;
+
+console.log("Valor de x:", x);
+
+/*Ou variáveis:*/
+
+let a = 2;
+let b = 3;
+let sum = a + b;
+
+console.log("Valor de sum:", sum);
+
+/*Ou expressões:*/
+
+let c = 2;
+let exp = (100 + 50) * c;
+
+console.log("Valor de exp:", exp);
